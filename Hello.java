cc5b692a046cf7ea0a32ef8d3841a131464bac82
ranjitk24 <ranjith.kssr5@gmail.com>
2024-09-18 11:49:15 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public static void main(String[] args)
 {
 System.out.println("First Java program, Printing message");
 System.out.println("Changed file, messge");
+  System.out.printLn("hello world");
 }
 }
